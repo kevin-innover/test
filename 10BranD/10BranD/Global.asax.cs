6198e178e5bb377bd163fa3cb46c50c9d96103ce
Kevin.yan <kevin.yan@3vr.com>
2017-05-25 15:18:15 +0800

    log the appliacation Error

--- 10BranD/10BranD/Global.asax.cs
+++ 10BranD/10BranD/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -30,8 +31,50 @@ namespace BranD10
 
         void Application_Error(object sender, EventArgs e)
         {
-            // 在出现未处理的错误时运行的代码
-          
+            // 在出现未处理的错误时运行的代码 
+            Exception ex = Server.GetLastError().GetBaseException();
+            Log.logger.Error("Application_Error", ex);
+
+            StringBuilder str = new StringBuilder();
+            if (Session != null && Session[CommonMethod.S_UserName] != null)
+            {
+                str.Append("\r\n.客户信息：" + Session[CommonMethod.S_UserName].ToString());
+            }
+
+            string ip = "";
+            if (Request.ServerVariables.Get("HTTP_X_FORWARDED_FOR") != null)
+            {
+                ip = Request.ServerVariables.Get("HTTP_X_FORWARDED_FOR").ToString().Trim();
+            }
+            else
+            {
+                ip = Request.ServerVariables.Get("Remote_Addr").ToString().Trim();
+            }
+            str.Append("\r\n\tIp:" + ip);
+            str.Append("\r\n\t浏览器:" + Request.Browser.Browser.ToString());
+            str.Append("\r\n\t浏览器版本:" + Request.Browser.MajorVersion.ToString());
+            str.Append("\r\n\t操作系统:" + Request.Browser.Platform.ToString());
+            str.Append("\r\n.错误信息：");
+            str.Append("\r\n\t页面：" + Request.Url.ToString());
+            str.Append("\r\n\t错误信息：" + ex.Message);
+            str.Append("\r\n\t错误源：" + ex.Source);
+            str.Append("\r\n\t异常方法：" + ex.TargetSite);
+            str.Append("\r\n\t堆栈信息：" + ex.StackTrace);
+            str.Append("\r\n--------------------------------------------------------------------------------------------------");
+            //创建路径 
+            string upLoadPath = Server.MapPath("~/log/");
+            if (!System.IO.Directory.Exists(upLoadPath))
+            {
+                System.IO.Directory.CreateDirectory(upLoadPath);
+            }
+            //创建文件 写入错误 
+            System.IO.File.AppendAllText(upLoadPath + DateTime.Now.ToString("yyyy.MM.dd") + ".log", str.ToString(), System.Text.Encoding.UTF8);
+            //处理完及时清理异常 
+            Server.ClearError();
+            //跳转至出错页面 
+            //  Response.Redirect("~/error.html");
+
+            Log.ErrorFormat(str .ToString());
 
         }
 
@@ -39,8 +82,8 @@ namespace BranD10
         {
             Session[CommonMethod.S_UserID] = 1;
             Session[CommonMethod.S_UserName] = "username";
-           // Session[CommonMethod.S_User] = DB.Context.From<Model.Brand>().Where(p => p.Id == 3).First();
-            
+            // Session[CommonMethod.S_User] = DB.Context.From<Model.Brand>().Where(p => p.Id == 3).First();
+
             // 在新会话启动时运行的代码
 
         }
